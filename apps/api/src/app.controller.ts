5611cfa80dc6efa6dfb78a2ae056a69f8294d681
manoj.pandey@javra.com <manoj.pandey@javra.com>
2023-02-26 22:48:08 +0545

    setup auth microservice and api gateway

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(@Inject('AUTH_SERVICE') private authService: ClientProxy) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getUser() {
+    return this.authService.send(
+      {
+        cmd: 'get-user',
+      },
+      {},
+    );
   }
 }
